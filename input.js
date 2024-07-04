a4bf996d3a76803e05f6be51c4a5bbee124f1ccd
Mathew M <mathewmastromonaco@gmail.com>
2024-07-04 00:46:03 -0400

    looped the directions and message keys

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 let connection;
+let moveInterval;
 
 const setupInput = function(conn)  {
   connection = conn;
@@ -15,7 +16,38 @@ const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
-  if (key === 'w') {
+
+  const directions = {
+    'w': 'Move: up',
+    'a': 'Move: left',
+    's': 'Move: down',
+    'd': 'Move: right'
+  };
+
+  const messages = {
+    '1': 'Say: Hey!',
+    '2': 'Say: Ssssneaky sssnake!',
+    '3': 'Say: Top Gap',
+    '4': 'Say: Mid Diff'
+  };
+
+  if (directions[key]) {
+    if (moveInterval) {
+      clearInterval(moveInterval);
+    }
+    connection.write(directions[key]);
+    moveInterval = setInterval(() => {
+      connection.write(directions[key]);
+    }, 50); // Adjust the interval as needed
+  }
+
+  if (messages[key]) {
+    connection.write(messages[key]);
+  }
+};
+
+
+/*  if (key === 'w') {
     connection.write('Move: up');
   }
   if (key === 'a') {
@@ -27,5 +59,5 @@ const handleUserInput = function(key) {
   if (key === 'd') {
     connection.write('Move: right');
   }
-};
+};*/
 module.exports = setupInput;
