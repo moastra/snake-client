b230d7f396e53ccec99297c615e42d0aee16924f
Mathew M <mathewmastromonaco@gmail.com>
2024-07-03 23:42:17 -0400

    added a connection message and a username to snek

--- client.js
+++ client.js
@@ -10,6 +10,12 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // connection message
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: MAT');
+  });
+
   // handles incoming data from the server
   conn.on('data', (data) => {
     console.log('Server says: ', data);
